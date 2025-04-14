1566593829182bfe9f45761d83d92a7f76561eaf
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-04-14 16:16:38 +0200

    Saving before merge

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,16 @@
-// import { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { useLocalStorageWatchlist } from "./Scripts/useLocalStorageWatchlist";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import MoviePage from "./Pages/MoviePage/MoviePage";
 import SearchPage from "./Pages/SearchPage/SearchPage";
 import StartPage from "./Pages/StartPage/StartPage";
 import WatchListPage from "./Pages/WatchListPage/WatchListPage";
+import { useLocalStorageWatchlist } from "./Scripts/useLocalStorageWatchlist";
 
 function App() {
+  const { watchlist, addToWatchlist, removeFromWatchlist } = useLocalStorageWatchlist();
+
   const router = createBrowserRouter([
     { path: "/", element: <StartPage />, errorElement: <ErrorPage /> },
     { path: "/movie:id", element: <MoviePage />, errorElement: <ErrorPage /> },
