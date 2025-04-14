63198eb3b887f7be4660a5833611ba81fd690f84
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-04-14 11:09:47 +0200

    Added assets and root variables

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="app"></div>
-  
-  )
+    <div className="app">
+      <h1>h1 kommer här</h1>
+      <h2>h2 kommer här</h2>
+      <h3>h3 kommer här</h3>
+      <h4>h4 kommer här</h4>
+      <h5>h5 kommer här</h5>
+      <p>p kommer här</p>
+      <span>span kommer här</span>
+      <img src="src/assets/logo-the-turtlebase.svg" alt="The Turtlebase" />
+      <img className="error__turtle" src="src/assets/404-turtle.png" alt="The Turtlebase" />
+    </div>
+  );
 }
 
-export default App
+export default App;
