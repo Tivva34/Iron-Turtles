9e383c188d740dd00a786ef3e12e0251579eed85
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-04-15 08:33:41 +0200

    Errormsg shown if the watchlist is empty

--- vite-project/src/Pages/WatchListPage/WatchListPage.jsx
+++ vite-project/src/Pages/WatchListPage/WatchListPage.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./watchListPage.css";
 import { useOutletContext } from "react-router-dom";
 import List from "../../Components/List";
+import ErrorMsg from "../../Components/ErrorMsg";
 
 function WatchListPage(testArr) {
   const { watchlist, addToWatchlist, removeFromWatchlist } = useOutletContext();
   console.log(watchlist);
+
   return (
     <section>
       <h1>WatchListPage</h1>
-      {watchlist && <List arr={watchlist} />}
+      {watchlist.length > 0 ? (
+        <List arr={watchlist} />
+      ) : (
+        <ErrorMsg title={"This is not a great list. In fact, it's not even a list."} text={"Let's fill it up!"} />
+      )}
     </section>
   );
 }
