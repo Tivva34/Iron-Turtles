4cabcb3281d189b8a5748205f6da8eb66e1126bb
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-04-15 07:56:44 +0200

    Fetch moved from List to StartPage. Id added to each movie.

--- vite-project/src/Pages/StartPage/StartPage.jsx
+++ vite-project/src/Pages/StartPage/StartPage.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import List from '../../Components/List';
-import './startPage.css';
-import testMovies from '../../data/testMovies';
-import MovieCarousel from '../../Components/MovieCarousel/MovieCarousel';
+import React from "react";
+import List from "../../Components/List";
+import "./startPage.css";
+import testMovies from "../../data/testMovies";
+import MovieCarousel from "../../Components/MovieCarousel/MovieCarousel";
+import { useFetch } from "../../Scripts/useFetch";
+
+//  "Fisher-Yates shuffle-algorit" funktion för att slumpa ordningen på arrayen
+const shuffleArray = (array) => {
+  if (!Array.isArray(array)) return []; // Om arrayen inte är en array, returnera en tom array
+  const shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Byt plats på elementen
+  }
+  return shuffledArray;
+};
 
 function StartPage() {
-	return (
-		<>
-			<h1>Startpage</h1>
-			<MovieCarousel />
-			<List movieList={testMovies} />
-		</>
-	);
+  const url = "https://santosnr6.github.io/Data/favoritemovies.json";
+  const { data, isLoading, isError } = useFetch(url);
+
+  const addIdToArr = data.map((obj, index) => ({ ...obj, id: index }));
+  console.log(addIdToArr);
+
+  // Hantera laddning och fel
+  if (isLoading) {
+    return <section>Loading...</section>;
+  }
+
+  if (isError) {
+    return <section>Something went wrong!</section>;
+  }
+
+  // Slumpa ordningen på filmerna om data finns och är en array
+  const shuffledMovies = shuffleArray(addIdToArr);
+
+  return (
+    <section className="page">
+      <h1>Startpage</h1>
+      <MovieCarousel />
+      {addIdToArr && <List arr={shuffledMovies} />}
+    </section>
+  );
 }
 
 export default StartPage;
