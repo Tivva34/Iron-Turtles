0d1bd7799572769952122b09b730b14bdf5adfae
Robin-Torp <3kdyhfe24.torpro@folkuniversitetet.nu>
2025-04-15 18:44:03 +0200

    Commit för att dra ner dev

--- vite-project/src/Components/MovieCardSmall/MovieCardSmall.jsx
+++ vite-project/src/Components/MovieCardSmall/MovieCardSmall.jsx
@@ -3,37 +3,45 @@ import Button from "../Button";
 import { useOutletContext } from "react-router-dom";
 
 function MovieCardSmall({ movie }) {
-  const { watchlist, addToWatchlist, removeFromWatchlist } = useOutletContext(); //Outlet används i RootLayout används därför här för att "packa upp" innehållet som kommer med från RootLayout via App.jsx
+	const { watchlist, addToWatchlist, removeFromWatchlist } = useOutletContext();
 
-  const isInWatchlist = (book) => {
-    if (watchlist.some((b) => b.id === book.id)) return true;
-    else return false;
-  };
+	const isInWatchlist = (movie) => {
+		return watchlist.some((b) => b.id === movie.id);
+	};
 
-  return (
-    <>
-      <div id={movie.id} className="list-item__div">
-        <Link className="nav-link">
-          <li className="list-item__li">
-            <div className="list-item__img-div">
-              <img src={movie.Poster} alt={`Poster of ${movie.Title}`} className="list-item__img" />
-            </div>
-            <h3 className="list-item__title">{movie.Title}</h3>
-          </li>
-        </Link>
-        {isInWatchlist(movie) ? (
-          <Button
-            id={"smallCardBtn"}
-            className={"errorpage__button lite-item__button"}
-            onClickFunction={() => removeFromWatchlist(movie.id)}
-            btntext={"Remove"}
-          />
-        ) : (
-          <Button id={"smallCardBtn"} className={"errorpage__button lite-item__button"} onClickFunction={() => addToWatchlist(movie)} btntext={"Save"} />
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div id={movie.id} className="list-item__div">
+				<li className="list-item__li">
+					<Link to={`/movie/${movie.imdbID}`} className="list-item__link">
+						<div className="list-item__img-div">
+							<img
+								src={movie.Poster}
+								alt={`Poster of ${movie.Title}`}
+								className="list-item__img"
+							/>
+						</div>
+					</Link>
+					<h3 className="list-item__title">{movie.Title}</h3>
+				</li>
+				{isInWatchlist(movie) ? (
+					<Button
+						id={"smallCardBtn"}
+						className={"errorpage__button lite-item__button"}
+						onClickFunction={() => removeFromWatchlist(movie.id)}
+						btntext={"Remove"}
+					/>
+				) : (
+					<Button
+						id={"smallCardBtn"}
+						className={"errorpage__button lite-item__button"}
+						onClickFunction={() => addToWatchlist(movie)}
+						btntext={"Save"}
+					/>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default MovieCardSmall;
