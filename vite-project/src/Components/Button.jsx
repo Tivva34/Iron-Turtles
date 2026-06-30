d3dada78c9ddb56f03b8bc812c420916846c765b
Tivva34 <tivva91@gmail.com>
2026-06-30 19:21:53 +0200

    Renamed folder and deplyed gh-pages

--- vite-project/src/Components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ id, className, onClickFunction, btntext }) {
-  return (
-    <button id={id} className={`page__button ${className}`} onClick={onClickFunction}>
-      {btntext}
-    </button>
-  );
-}
-
-export default Button;
